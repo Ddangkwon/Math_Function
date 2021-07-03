6e9ffd0118d99d79d5b7be2cd48421a6fa42c997
yeongkwonko <dram109@naver.com>
2021-07-03 22:34:29 +0900

    [DEV]Add math function and print to text file.

--- Calc_Comp.c
+++ Calc_Comp.c
@@ -1,31 +1,75 @@
 #include "Typedef.h"
 
+
+/***********************************************************************************************
+ * Global Variables
+ ***********************************************************************************************/
+complex64_t a = {2.4, 3.6};
+complex64_t b = {12.0, 42.0};
+complex64_t result = {0.0f, 0.0f};
+uint32_t calc_type = 3;
+byteTypeConv parseBytes;
+/***********************************************************************************************
+ * Main
+ ***********************************************************************************************/
 int main()
 {
-    complex64_t a = {8.0, 5.0};
-    complex64_t b = {4.0, 2.0};
-    complex64_t result = {0.0f, 0.0f};
-    uint32_t calc_type = 3;
-    switch(calc_type)
+    FILE *fp;
+    fp = fopen("output_log.txt", "w");
+    if(fp == NULL)
+    {
+        printf("input file read fail!!\n");
+        return -1;
+    }
+    else
+    {
+        printf("input file read success..\n");
+    }
+
+    result = compCalculator(0, a, b);
+    fprintf(fp, "SUM\n");
+    fprintf(fp, "real : %.8f, imag : %.8f\n", result.real, result.imag);
+    result = compCalculator(1, a, b);
+    fprintf(fp, "SUB\n");
+    fprintf(fp, "real : %.8f, imag : %.8f\n", result.real, result.imag);
+    result = compCalculator(2, a, b);
+    fprintf(fp, "MUL\n");
+    fprintf(fp, "real : %.8f, imag : %.8f\n", result.real, result.imag);
+    result = compCalculator(3, a, b);
+    fprintf(fp, "DIV\n");
+    fprintf(fp, "real : %.8f, imag : %.8f\n", result.real, result.imag);
+
+    fclose(fp);
+    return 0;
+}
+
+/***********************************************************************************************
+ * Function Declaration
+ ***********************************************************************************************/
+
+complex64_t compCalculator(uint32_t calcType, complex64_t a, complex64_t b)
+{
+    complex64_t z;
+    switch(calcType)
     {
         case ADD:
-            result = addComp(&a, &b);
+            z = addComp(&a, &b);
             break;
         case SUB:
-            result = subComp(&a, &b);
+            z = subComp(&a, &b);
             break;
         case MUL:
-            result = mulComp(&a, &b);
+            z = mulComp(&a, &b);
             break;
         case DIV:
-            result = divComp(&a, &b);
+            z = divComp(&a, &b);
             break;
         default:
             /* NOP */
             break;
     }
-    printf("real : %f, imag : %f\n", result.real, result.imag);
-    return 0;
+    printf("real : %f, imag : %f\n", z.real, z.imag);
+    return z;
 }
 complex64_t addComp(const complex64_t *z1, const complex64_t *z2)
 {
@@ -75,5 +119,37 @@ complex64_t conjComp(const complex64_t *z1)
     complex64_t output;
     output.real = z1->real;
     output.imag = -1 * z1->imag;
+    return output;
+}
+
+float32_t absComp(const complex64_t *z1)
+{
+    float32_t output = sqrt((z1->real * z1->real) + (z1->imag * z1->imag));
+    return output;
+}
+
+float32_t degToRad(float32_t angle)
+{
+    angle = angle / 180.0 * M_PI;
+    return angle;
+}
+float32_t radToDeg(float32_t angle)
+{
+    angle = angle / M_PI * 180.0;
+    return angle;
+}
+
+complex64_t phaseToComp(float32_t phase)
+{
+    complex64_t output;
+    output.real = cos(phase);
+    output.imag = sin(phase);
+    return output;
+}
+
+float32_t compToPhase(complex64_t z)
+{
+    float32_t output;
+    output = atan2(z.imag, z.real);
     return output;
 }
--- Typedef.h
+++ Typedef.h
@@ -1,6 +1,6 @@
 #ifndef TYPEDEF_H
 #define TYPEDEF_H
-
+#define _USE_MATH_DEFINES
 #include <stdint.h>
 #include <stdio.h>
 #include <math.h>
@@ -22,9 +22,26 @@ typedef enum
     DIV
 }calc_complex_e;
 
+typedef union
+{
+    uint8_t     _uc[4];
+    uint16_t    _us[2];
+    uint32_t    _ui;
+    float32_t   _f;
+    int8_t      _sc[4];
+    int16_t     _ss[2];
+    int32_t     _si;
+}byteTypeConv;
+complex64_t compCalculator(uint32_t calcType, complex64_t a, complex64_t b);
 complex64_t addComp(const complex64_t *z1, const complex64_t *z2);
 complex64_t subComp(const complex64_t *z1, const complex64_t *z2);
 complex64_t mulComp(const complex64_t *z1, const complex64_t *z2);
 complex64_t divComp(const complex64_t *z1, const complex64_t *z2);
 complex64_t conjComp(const complex64_t *z1);
+float32_t absComp(const complex64_t *z1);
+
+float32_t degToRad(float32_t angle);
+float32_t radToDeg(float32_t angle);
+complex64_t phaseToComp(float32_t phase);
+float32_t compToPhase(complex64_t z);
 #endif
